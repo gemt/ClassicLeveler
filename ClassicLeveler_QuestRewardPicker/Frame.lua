f0706dc8cf6a93cc33ebdfe8a8aa2839ecb516e8
skarseth <skarsethkristian@gmail.com>
2019-04-28 00:09:46 +0200

    add a IsQuestComplete(qname) function

--- ClassicLeveler_QuestRewardPicker/Frame.lua
+++ ClassicLeveler_QuestRewardPicker/Frame.lua
@@ -306,6 +306,30 @@ function QRPF_AddonLoaded()
 	
 end
 
+function IsQuestComplete(qtitle)
+	local objectives = nil   
+	local e = GetNumQuestLogEntries()
+	for q=2, e do
+        if qtitle == GetQuestLogTitle(q) then
+			local n = GetNumQuestLeaderBoards(q)
+			for x=1, n do
+				local l = GetQuestLogLeaderBoard(x, q)    
+                if string.find(l, "%d+/%d+") ~= nil then
+					local a = string.sub(l, string.find(l, "%d+/%d+"))
+					local b = string.sub(a, string.find(a, "%d+"))
+					local c = string.sub(a, string.find(a, "%d+$"))
+                    if b ~= c then 
+						return 0 -- one objective is not complete. Return 0
+                    end                                                         
+                end
+			end
+			return 1 -- all objectives were n/n
+		end
+	end
+	_print(qtitle.." not found")
+    return nil
+end
+
 CLIgnoreQuests = {
 	[""] = {Objective=""},
 }
