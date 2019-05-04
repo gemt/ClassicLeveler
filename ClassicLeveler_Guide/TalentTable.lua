7dfd85b2ddf9122b68332e26bd37ce3f0dd2bfbb
skarseth <skarsethkristian@gmail.com>
2019-05-05 00:34:23 +0200

    move talentTable and vendorList to separate files

---
+++ ClassicLeveler_Guide/TalentTable.lua
@@ -0,0 +1,3 @@
+-- Author      : G3m7
+-- Create Date : 5/4/2019 9:44:32 PM
+
---
+++ ClassicLeveler_Guide/VendorList.lua
@@ -0,0 +1,6 @@
+-- Author      : G3m7
+-- Create Date : 5/4/2019 9:44:47 PM
+
+CLGuide_VendorList = {
+"Light Feather"
+} 
