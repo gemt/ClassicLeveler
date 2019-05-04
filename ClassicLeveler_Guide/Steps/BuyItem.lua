7d2e598d043cd0a051aab6a032f70693fa55f693
skarseth <skarsethkristian@gmail.com>
2019-05-04 13:40:22 +0200

    BuyItem tested and working. Added check that you have enough money

--- ClassicLeveler_Guide/Steps/BuyItem.lua
+++ ClassicLeveler_Guide/Steps/BuyItem.lua
@@ -5,11 +5,15 @@ local function OnMerchantShow()
 	if invCount < CLGuide_CurrentStep.BuyItem.Count then
 		local itemCountToBuy = CLGuide_CurrentStep.BuyItem.Count - invCount
 		for i=1, GetMerchantNumItems() do
-			local itmName,_,_,quantity = GetMerchantItemInfo(i)
+			local itmName,_,price,quantity = GetMerchantItemInfo(i)
 			if itmName == CLGuide_CurrentStep.BuyItem.Item then
 				-- stackable items like arrows cant be bought one by one (as in, you cant get less than 200)
 				-- but we let the itemCountToBuy/quantity round down to never buy MORE items than specified
 				itemCountToBuy = itemCountToBuy/quantity
+				if itemCountToBuy*price > GetMoney() then
+					GuidePrint("You do not have enough money to buy the required items")
+					return
+				end
 				BuyMerchantItem(i, itemCountToBuy)
 				CLGuide_CompleteCurrentStep()
 				break
@@ -23,8 +27,8 @@ function CLGuide_BuyItem()
 	if UnitName("target") ~= CLGuide_CurrentStep.BuyItem.Npc then return end
 
 	if event == "MERCHANT_SHOW" then
-		SelectGossipOption(GetGossipIndex("vendor"))
-	elseif event == "GOSSIP_SHOW" then 
 		OnMerchantShow()
+	elseif event == "GOSSIP_SHOW" then 
+		SelectGossipOption(CLGuide_GetGossipIndex("vendor"))
 	end
 end
