0b1e7687036d06f845a82306b33cc885b0622d18
skarseth <skarsethkristian@gmail.com>
2019-05-04 17:16:30 +0200

    syntaxerror fix in levelreached trigger

--- ClassicLeveler_Guide/Steps/LevelReached.lua
+++ ClassicLeveler_Guide/Steps/LevelReached.lua
@@ -1,7 +1,10 @@
 
 function CLGuide_LevelReached()
-	if CLGuide_CurrentStep.Lvl ~= nil then
+    -- not bothering checking which event we get here. Just check it on anything
+
+	if CLGuide_CurrentStep.Lvl == nil then return end
     if UnitLevel("player") < CLGuide_CurrentStep.Lvl.lvl then return end
+
     if CLGuide_CurrentStep.xp ~= nil then
         if UnitXP("player") < CLGuide_CurrentStep.xp then return end
     end
