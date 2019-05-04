ed3da21efc71fb718096f0ce701be9eb257598b7
skarseth <skarsethkristian@gmail.com>
2019-05-04 13:40:05 +0200

    TakeTaxi fixes, working

--- ClassicLeveler_Guide/Steps/TakeTaxi.lua
+++ ClassicLeveler_Guide/Steps/TakeTaxi.lua
@@ -6,19 +6,19 @@ function CLGuide_TakeTaxi()
 	-- todo: handle CLGuide_CurrentStep.ReqPrev. Dont wanna fly away automatically if we havent completed prevstep!
 	
 	if event == "GOSSIP_SHOW" then
-		for i=1, NumTaxiNodes() do
-			if string.lower(TaxiNodeName(i)) == string.lower(CLGuide_CurrentStep.Taxi) then
-				TakeTaxiNode(i)
-				CLGuide_CompleteCurrentStep()
-			end
-		end
-	elseif event == "TAXIMAP_OPENED" then
 		local taxiIdx = CLGuide_GetGossipIndex("taxi")
 		if taxiIdx ~= nil then
 			-- TODO: CHECK THAT LAST N STEPS ARE PROPERLY COMPLETED BEFORE FLYING AWAY!!!
 			-- ALTERNATIVELY, or additionally, popupbox with confirm button 
 			SelectGossipOption(taxiIdx)
 		end
+	elseif event == "TAXIMAP_OPENED" then
+		for i=1, NumTaxiNodes() do
+			if string.lower(TaxiNodeName(i)) == string.lower(CLGuide_CurrentStep.Taxi) then
+				TakeTaxiNode(i)
+				CLGuide_CompleteCurrentStep()
+				return
+			end
+		end
 	end
-	
 end
