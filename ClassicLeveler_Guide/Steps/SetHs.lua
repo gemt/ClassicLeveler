f88f904652715675778d4058b714d3062073a53b
skarseth <skarsethkristian@gmail.com>
2019-05-04 18:59:27 +0200

    fix setHS to check CONFIRM_BINDER event

--- ClassicLeveler_Guide/Steps/SetHs.lua
+++ ClassicLeveler_Guide/Steps/SetHs.lua
@@ -1,8 +1,13 @@
 
 function CLGuide_SetHs()
 	if CLGuide_CurrentStep.SetHs == nil then return end
-	if event ~= "GOSSIP_SHOW" then return end
-	if UnitName("target") ~= CLGuide_CurrentStep.SetHs then return end
 	
-	ConfirmBinder() -- TODO: in 1.12, we can call this without selecting gossip option. Test on retail
+	if event == "GOSSIP_SHOW" then 
+        if UnitName("target") ~= CLGuide_CurrentStep.SetHs then return end
+        SelectGossipOption(CLGuide_GetGossipIndex("binder"))
+    elseif event == "CONFIRM_BINDER" then
+        if UnitName("target") ~= CLGuide_CurrentStep.SetHs then return end
+	    ConfirmBinder()
+    end
+
 end
