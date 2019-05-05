f8f87d292f8ea58297d4e07df6c8017bed6d1f29
skarseth <skarsethkristian@gmail.com>
2019-05-05 15:35:04 +0200

    add support for placing spells on actionbars after training

--- ClassicLeveler_Guide/Steps/TrainSkill.lua
+++ ClassicLeveler_Guide/Steps/TrainSkill.lua
@@ -1,3 +1,38 @@
+--[[
+PlaceAction(actionbarindex)
+ClearCursor()
+
+bookType String - Either BOOKTYPE_SPELL ("spell") or BOOKTYPE_PET ("pet").
+]]
+CLGuide_PlaceSpellQueue = {}
+-- automatically finds the highest rank of the spell
+function CLGuide_GetSpellBookId(spellname)
+    local a = 1
+    local b = GetSpellName(a, "BOOKTYPE_SPELL")
+    local c = nil
+    while b do
+	    if b == spellname and GetSpellName(a+1, "BOOKTYPE_SPELL") ~= spellname then 
+            c = a
+		end
+        a = a + 1
+        b = GetSpellName(a, "BOOKTYPE_SPELL")
+	end
+	return c
+end    
+
+
+function CLGuide_PlaceSpell(spellname, actionBarIndex)
+    local spellIdx = CLGuide_GetSpellBookId(spellname)
+    if spellIdx == nil then 
+        GuidePrint("CLGuide_PlaceSpell spell <"..spellname.."> not found.")
+        return 
+    end
+    ClearCursor()
+    PickupSpell(spellIdx, "BOOKTYPE_SPELL")
+    PlaceAction(actionBarIndex)
+    ClearCursor()
+end
+
 
 local function OnTrainerShow()
     -- setting filters such that we don't see unavailable spells (cheaper), 
@@ -14,9 +49,14 @@ local function OnTrainerShow()
             rank = string.lower(rank)
             if category == "available" then
                 for gidx = 1, getn(CLGuide_CurrentStep.TrainSkill) do
-                    if string.lower(CLGuide_CurrentStep.TrainSkill[gidx].n) == name then
-                        if CLGuide_CurrentStep.TrainSkill[gidx].r == nil or string.lower(CLGuide_CurrentStep.TrainSkill[gidx].r) == rank then
+                    local stepskill = CLGuide_CurrentStep.TrainSkill[gidx]
+                    if string.lower(stepskill.n) == name then
+                        if stepskill.r == nil or string.lower(stepskill.r) == rank then
                             BuyTrainerService(spellIdx)
+                            if stepskill.Actionbar ~= nil then
+                                -- queueing delayed placement on actionbars
+                                table.insert(CLGuide_PlaceSpellQueue, {Spell=stepskill,Time=GetTime()+2})
+                            end
                             table.remove(CLGuide_CurrentStep.TrainSkill, gidx)
                             break;
                         end
@@ -27,12 +67,25 @@ local function OnTrainerShow()
     end
     
     GuidePrint("Could not train the following spells:")
+
     for gidx = 1, getn(CLGuide_CurrentStep.TrainSkill) do
-        GuidePrint("    "..CLGuide_CurrentStep.TrainSkill[gidx].n.." ("..CLGuide_CurrentStep.TrainSkill[gidx].r..")")
+        if CLGuide_CurrentStep.TrainSkill[gidx].r ~= nil then
+            GuidePrint("    "..CLGuide_CurrentStep.TrainSkill[gidx].n.." ("..CLGuide_CurrentStep.TrainSkill[gidx].r..")")
+        else
+            GuidePrint("    "..CLGuide_CurrentStep.TrainSkill[gidx].n)
+        end
     end
     CLGuide_CompleteCurrentStep()
 end
 
+function CLGuide_TrainSkillOnUpdate()
+    for i=getn(CLGuide_PlaceSpellQueue), 1, -1 do
+        if GetTime() > CLGuide_PlaceSpellQueue[i].Time then
+            CLGuide_PlaceSpell(CLGuide_PlaceSpellQueue[i].Spell.n, CLGuide_PlaceSpellQueue[i].Spell.Actionbar)
+            table.remove(CLGuide_PlaceSpellQueue,i)
+        end
+    end
+end
 
 function CLGuide_TrainSkill()
     if CLGuide_CurrentStep.TrainSkill == nil then return end
