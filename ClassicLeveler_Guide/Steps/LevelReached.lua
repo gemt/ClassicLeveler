df247522e0d631647d1e46b80950c1c08606b450
skarseth <skarsethkristian@gmail.com>
2019-05-06 22:00:10 +0200

    fix various bugs in levelreached trigger. Requires PLAYER_XP_UPDATE event

--- ClassicLeveler_Guide/Steps/LevelReached.lua
+++ ClassicLeveler_Guide/Steps/LevelReached.lua
@@ -1,13 +1,16 @@
 
 function CLGuide_LevelReached()
-    -- not bothering checking which event we get here. Just check it on anything
     -- step wont autocomplete ON levelup event because UnitLevel("player") returns previous lvl during the event
     -- However, on the first event after levelup, we will always get the correct lvl
 	if CLGuide_CurrentStep.Lvl == nil then return end
-    if UnitLevel("player") < CLGuide_CurrentStep.Lvl.lvl then return end
-
-    if CLGuide_CurrentStep.xp ~= nil then
-        if UnitLevel("player") > CLGuide_CurrentStep.Lvl.lvl or UnitXP("player") < CLGuide_CurrentStep.xp then return end
+    
+    if event == "PLAYER_LEVEL_UP" and CLGuide_CurrentStep.Lvl.xp == nil then
+        if arg1 < CLGuide_CurrentStep.Lvl.lvl then return end
+        CLGuide_CompleteCurrentStep()
+    elseif event == "PLAYER_XP_UPDATE" and CLGuide_CurrentStep.Lvl.xp ~= nil then
+        if UnitLevel("player") < CLGuide_CurrentStep.Lvl.lvl then return end
+        if UnitXP("player") >= CLGuide_CurrentStep.Lvl.xp then
+            CLGuide_CompleteCurrentStep()
+        end
     end
-    CLGuide_CompleteCurrentStep()
 end
