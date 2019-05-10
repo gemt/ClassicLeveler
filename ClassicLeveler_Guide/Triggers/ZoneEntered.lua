078540c352dbd8db9918e12078109d653c325b75
skarseth <skarsethkristian@gmail.com>
2019-05-10 17:20:31 +0200

    add ZONE_CHANGED_INDOORS event too and handling in zoneentered. Though as noted in a comment, its not really working well in 1.12 since api functions return previous zone on event

--- ClassicLeveler_Guide/Triggers/ZoneEntered.lua
+++ ClassicLeveler_Guide/Triggers/ZoneEntered.lua
@@ -1,12 +1,21 @@
 
 -- ZONE_CHANGED_NEW_AREA new map basically (stv, duskwood, ashenvale, etc)
 -- ZONE_CHANGED subzones, i.e when the text on minimap changes
-
+-- None of this really works well (except ZONE_CHANGED_NEW_AREA) because the api functions returns the previous zone
+-- on event......
 function CLGuide_ZoneEntered()
-	if event ~= "ZONE_CHANGED_NEW_AREA" and event ~= "ZONE_CHANGED" then return end
 	if CLGuide_CurrentStepTable.Zone == nil then return end
-    	
-	if CLGuide_CurrentStepTable.Zone == GetRealZoneText() then
+	if      event ~= "ZONE_CHANGED_NEW_AREA" 
+        and event ~= "ZONE_CHANGED" 
+        and event ~= "ZONE_CHANGED_INDOORS" then 
+        return 
+    end
+    GuidePrint("4"..GetMinimapZoneText())
+    if event == "ZONE_CHANGED_NEW_AREA" and CLGuide_CurrentStepTable.Zone == GetRealZoneText() then
 		CLGuide_CompleteCurrentStep()
+    elseif event == "ZONE_CHANGED" and CLGuide_CurrentStepTable.Zone == GetMinimapZoneText() then
+        CLGuide_CompleteCurrentStep()
+    elseif event == "ZONE_CHANGED_INDOORS" and CLGuide_CurrentStepTable.Zone == GetMinimapZoneText() then
+        CLGuide_CompleteCurrentStep()
     end
 end
