e79b98d5a1dccdfe7f15942af04834f28fad85a8
skarseth <skarsethkristian@gmail.com>
2019-05-10 16:58:17 +0200

    register ZONE_CHANGED event and also handle  this in zoneEntered trigger to allow triggers on subzones

--- ClassicLeveler_Guide/Triggers/ZoneEntered.lua
+++ ClassicLeveler_Guide/Triggers/ZoneEntered.lua
@@ -1,10 +1,12 @@
 
+-- ZONE_CHANGED_NEW_AREA new map basically (stv, duskwood, ashenvale, etc)
+-- ZONE_CHANGED subzones, i.e when the text on minimap changes
+
 function CLGuide_ZoneEntered()
-	if event ~= "ZONE_CHANGED_NEW_AREA" then return end
+	if event ~= "ZONE_CHANGED_NEW_AREA" and event ~= "ZONE_CHANGED" then return end
 	if CLGuide_CurrentStepTable.Zone == nil then return end
-	
+    	
 	if CLGuide_CurrentStepTable.Zone == GetRealZoneText() then
 		CLGuide_CompleteCurrentStep()
-	end
-
+    end
 end
