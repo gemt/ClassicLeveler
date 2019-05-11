9d141d10d83431cea722cfa2f4a61b13506628fd
skarseth <skarsethkristian@gmail.com>
2019-05-11 21:45:05 +0200

    move kurzen pt1 bits to before first HS to BB

--- ClassicLeveler_Guide/Guides/34_36_Stv.lua
+++ ClassicLeveler_Guide/Guides/34_36_Stv.lua
@@ -60,7 +60,7 @@ CLGuide_Stv1 = {
         {Text="Kill Elza and loot to complete Bride of the Embalmer", Ct="Bride of the Embalmer"},
         -- delivering bride of the embalmer after HS to BB later
 
-        {Text="Run into STV", Zone="Stranglethorn Vale", 
+        {Text="Run into STV", Zone="Stranglethorn Vale", point={x=4474,y=8543},
             PinAdd="If Private Thorsen is not in Rebel Camp, he could be patrolling towards first bridge to the south. Potentially dead"},
         
 
@@ -83,7 +83,7 @@ CLGuide_Stv1 = {
 
 
         -- tiger/panther 1 + crocs
-        {Text="Accept Welcome to the Jungle", At="Welcome to the Jungle", point={x=3566,y=1053}},
+        {Text="Accept Welcome to the Jungle (Nesingwary camp)", At="Welcome to the Jungle", point={x=3566,y=1053}},
         {Text="Turn in Hemet Nesingwary", Dt={q="Hemet Nesingwary"}, point={x=3566,y=1081}},
         {Text="Turn in Welcome to the Jungle", Dt={q="Welcome to the Jungle"}, point={x=3566,y=1081}},
 
@@ -92,6 +92,16 @@ CLGuide_Stv1 = {
         {Text="Accept Panther Mastery", At="Panther Mastery", point={x=3555,y=1055}},
         
         {Text="Complete Tiger and Panther Mastery and Supply and Demand (crocolisks)", Mct={"Panther Mastery", "Tiger Mastery", "Supply and Demand"}},
+
+
+
+        -- do kurzen 1
+        {Text="Go to Kurzen => Deliver Bookie Herod (second floor of house)", Dt={q="Bookie Herod"}, point={x=4367,y=938}},
+        {Text="Accept The Hidden Key", At="The Hidden Key", point={x=4367,y=938}},
+
+        {Text="Complete Kurzen Quests: 15 Fighers, 7 Remedies + box(outside house, by fire or by stables)",
+            Mct={"Bad Medicine", "The Second Rebellion"}, PinAdd="Several basilisks for Singing Blue Shards E/NE of Kurzen"},
+
 
         {Text="Deliver Tiger Mastery", Dt={q="Tiger Mastery"}, point={x=3562,y=1062}},
         {Text="Accept Tiger Mastery pt2", At="Tiger Mastery", point={x=3562,y=1062}},
@@ -109,6 +119,8 @@ CLGuide_Stv1 = {
 
 
         -- hearthstone, get crocs pt2 and isle pt2
+        -- TODO: Not sure if HS will be ready in time here
+        -- IF IT IS NOT ready. Do some kurzen stuff when doing panther pt1?
         {Text="Hearthstone to Booty Bay", UseItem="Hearthstone", Proximity=20, point={x=2704,y=7735}},
         
         {Text="Deliver Investigate the Camp (third floor)", Dt={q="Investigate the Camp"}, point={x=2694,y=7721}},
@@ -124,8 +136,17 @@ CLGuide_Stv1 = {
 
         -- fly darkshire, deliver bride of embalmer
         {Text="Fly to Darkshire", Taxi="Darkshire, Duskwood", point={x=2753,y=7778}},
-        {Text="Turn inn Bride of the Embalmer", Dt={q="Bride of the Embalmer", Item="Mantle of Honor", Use=1, Vendor="Silk Mantle of Gamn"}, point={x=7193,y=4642}},
+        {Text="Turn inn Bride of the Embalmer (town hall)", Dt={q="Bride of the Embalmer", Item="Mantle of Honor", Use=1, Vendor="Silk Mantle of Gamn"}, point={x=7193,y=4642}},
         {Text="Run into STV, to the Rebel Camp", Zone="Stranglethorn Vale"},
+
+
+
+         -- deliver kurzen 1, get kurzen 2
+        {Text="Deliver The Second Rebellion (Rebel Camp)", Dt={q="The Second Rebellion"}, point={x=3802,y=333}},
+        {Text="Deliver Bad Medicine (Take shoulders if don't have Mantle of Honor)", Dt={q="Bad Medicine"}, point={x=3802,y=333}},
+        {Text="Accept Special Forces", At="Special Forces", point={x=3802,y=333}},
+        {Text="Accept Kurzen's Mystery", At="Kurzen's Mystery", point={x=3783,y=356}},
+        {Text="Buy Razor Arrows in Rebel Camp", BuyItem={Npc="Corporal Bluth", Item="Razor Arrow", Count=2800}, point={x=3796,y=298}},
 
 
 
@@ -140,14 +161,18 @@ CLGuide_Stv1 = {
         {Text="Accept Panther Mastery pt3", At="Panther Mastery", point={x=3555,y=1055}},
 
 
-        -- do kurzen 1
-        {Text="Go to Kurzen => Deliver Bookie Herod (second floor of house)", Dt={q="Bookie Herod"}, point={x=4367,y=938}},
-        {Text="Accept The Hidden Key", At="The Hidden Key", point={x=4367,y=938}},
 
-        {Text="Complete Kurzen Quests: 15 Fighers, 7 Remedies + box(outside house, by fire or by stables)",
-            Mct={"Bad Medicine", "The Second Rebellion"}, PinAdd="Several basilisks for Singing Blue Shards E/NE of Kurzen"},
+
+
+        -- do kurzen pt2
+        {Text="Deliver The Hidden Key (in the cave)", Dt={q="The Hidden Key"}, point={x=4962,y=756}},
+        {Text="Accept The Spy Revealed!", At="The Spy Revealed!", point={x=4962,y=756}},
+        {Text="Complete Special Forces (Kurzen cave)", Ct="Special Forces", point={x=4560,y=826}},
+        
+
         
         {Text="Complete Tiger Mastery pt2 if did not already. Many south of Kurzen", Ct="Tiger Mastery", point={x=4683,y=1457}},
+
 
 
         -- oil rig/goblin stuff, panther 3, croc2, raptor 3 and tiger 3
@@ -162,7 +187,7 @@ CLGuide_Stv1 = {
         {Text="Complete Tiger Mastery pt3 north of Grom'Gol", Ct="Tiger Mastery", point={x=3153,y=1652},
             PinAdd="If still on pt2, deliver it first, then do pt3. Also check for Basilisks again"},
 
-        {Text="Clear out Singing Blue Shards bazilisks (below raptor pt1) on the way back to Nessingwary camp", Ct="Singing Blue Shards", point={x=3565,y=1069},
+        {Text="Clear out Singing Blue Shards bazilisks (below raptor pt1) on the way back to Nessingwary camp", Ct="Singing Blue Shards", point={x=2849,y=1762},
             PinAdd="Getting another pass by the basilisks later, so dont wait for respawn"},
 
         
@@ -174,26 +199,9 @@ CLGuide_Stv1 = {
         
         {Text="Deliver Panther Mastery pt3 (skip pt4 for later)", Dt={q="Panther Mastery"}, point={x=3555,y=1055}},
 
-
-
-        -- deliver kurzen 1, get kurzen 2
-        {Text="Deliver The Second Rebellion (Rebel Camp)", Dt={q="The Second Rebellion"}, point={x=3802,y=333}},
-        {Text="Deliver Bad Medicine (Take shoulders if don't have Mantle of Honor)", Dt={q="Bad Medicine"}, point={x=3802,y=333}},
-        {Text="Accept Special Forces", At="Special Forces", point={x=3802,y=333}},
-        {Text="Accept Kurzen's Mystery", At="Kurzen's Mystery", point={x=3783,y=356}},
-
-        {Text="Accept Kurzen's Mystery", At="Kurzen's Mystery", point={x=3783,y=356}},
-        {Text="Buy Razor Arrows in Rebel Camp", BuyItem={Npc="Corporal Bluth", Item="Razor Arrow", Count=2800}, point={x=3796,y=298}},
-
-        -- do kurzen pt2
-        {Text="Deliver The Hidden Key (in the cave)", Dt={q="The Hidden Key"}, point={x=4962,y=756}},
-        {Text="Accept The Spy Revealed!", At="The Spy Revealed!", point={x=4962,y=756}},
-        {Text="Complete Special Forces (Kurzen cave)", Ct="Special Forces", point={x=4560,y=826}},
         
-
-        -- tiger pt4
+        -- tiger 4
         {Text="Complete Tiger Mastery pt4 (Sin'Dall, SW)", Ct="Tiger Mastery", point={x=3222,y=1730}},
-        {Text="Deliver Tiger Mastery pt4", Dt={q="Tiger Mastery"}, point={x=3562,y=1062}},
 
         -- tablets, isle pt2, troll necks, blue shards
         {Text="Get first Troll legend for Kurzen's Mystery (closest camp to Grom'Gol)", Item={Name="The First Troll Legend", Count=1}, point={x=2946,y=1913},
@@ -205,29 +213,31 @@ CLGuide_Stv1 = {
 
         {Text="Complete Water Elementals at the isle", Ct="Water Elementals", point={x=2029,y=2151}},
         
-        {Text="Get Third Troll legend for Kurzen's Mystery (out by murlocs)", Item={Name="The Third Troll Legend", Count=1}, point={x=2294,y=1198},
+        {Text="Get Third Troll legend for Kurzen's Mystery (SW part of trollplace)", Item={Name="The Third Troll Legend", Count=1}, point={x=2294,y=1198},
             PinAdd="Last chance to finish Singing Blue Shards now"},
         {Text="Get Fourth Troll legend for Kurzen's Mystery (out by murlocs)", Item={Name="The Fourth Troll Legend", Count=1}, point={x=2473,y=891}},
         
         {Text="Complete Bloodscalp Ears here before leaving", Ct="Bloodscalp Ears"},
 
+        -- tiger 4
+        {Text="Deliver Tiger Mastery pt4", Dt={q="Tiger Mastery"}, point={x=3562,y=1062}},
         
         -- deliver kurzen 2 + tablet
-        {Text="Deliver Special Forces", Dt={q="Special Forces"}, point={x=3802,y=333}},
-        {Text="Deliver The Spy Revealed!", Dt={q="The Spy Revealed!"}, point={x=3802,y=333}},
+        {Text="Deliver Special Forces (skip kurzen pt3)", Dt={q="Special Forces"}, point={x=3804,y=301}},
+        {Text="Deliver The Spy Revealed!", Dt={q="The Spy Revealed!"}, point={x=3804,y=301}},
         
-        {Text="Accept Patrol Schedules", At="Patrol Schedules", point={x=3802,y=333}},
+        {Text="Accept Patrol Schedules", At="Patrol Schedules", point={x=3804,y=301}},
         {Text="Deliver Patrol Schedules!", Dt={q="Patrol Schedules"}, point={x=3767,y=339}},
-        {Text="Accept Report to Doren", At="Report to Doren", point={x=3802,y=333}},
-        {Text="Deliver Report to Doren", Dt={q="Report to Doren", Item="Junglewalker Sandals", Use=1}, point={x=3802,y=333}},
+        {Text="Accept Report to Doren", At="Report to Doren", point={x=3767,y=339}},
+        {Text="Deliver Report to Doren", Dt={q="Report to Doren", Item="Junglewalker Sandals", Use=1}, point={x=3804,y=301}},
 
-        {Text="Deliver Kurzen's Mystery", Dt={q="Kurzen's Mystery"}, point={x=3783,y=356}},
+        {Text="Deliver Kurzen's Mystery (skip troll witchery)", Dt={q="Kurzen's Mystery"}, point={x=3783,y=356}},
 
 
         -- HS BB, deliver remaining stuff
         {Text="Hearthstone to Booty Bay", UseItem="Hearthstone", Proximity=20, point={x=2704,y=7735}},
         {Text="Deliver Singing Blue Shards (next to innkeeper). Skip followup", Dt={q="Singing Blue Shards"}, point={x=2712,y=7721}},
-        {Text="Deliver Bloodscalp Ears", Dt={q="Bloodscalp Ears"}, point={x=2700,y=7713}},
+        {Text="Deliver Bloodscalp Ears (third floor)", Dt={q="Bloodscalp Ears"}, point={x=2700,y=7713}},
         {Text="Deliver Hostile Takeover", Dt={q="Hostile Takeover"}, point={x=2700,y=7713}},
 
         {Text="Deliver Goblin Sponsorship", Dt={q="Goblin Sponsorship"}, point={x=2722,y=7687}},
