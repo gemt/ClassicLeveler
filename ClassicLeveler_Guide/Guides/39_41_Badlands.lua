51d5be63b08b0f04120badbb0f1f7df3513801bd
skarseth <skarsethkristian@gmail.com>
2019-05-13 21:54:30 +0200

    finish up badlands with some deliveries and group notes

--- ClassicLeveler_Guide/Guides/39_41_Badlands.lua
+++ ClassicLeveler_Guide/Guides/39_41_Badlands.lua
@@ -7,6 +7,8 @@ CLGuide_Badlands = {
     Steps = {
     
         {Text="Fly to Thelsamar", Taxi="Thelsamar, Loch Modan", point={x=5548,y=4777}},
+        {Text="Set HS in Thelsamar", SetHs="Innkeeper Hearthstove", point={x=3553,y=4841}},
+
         {Text="Accept Badlands Reagent Run (in thelsamar)", At="Badlands Reagent Run", point={x=3707,y=4935}},
         {Text="Deliver Ironband Wants You! (Excavation site SE in Loch Modan)", Dt={q="Ironband Wants You!"}, point={x=6593,y=6562}},
         {Text="Accept Find Agmond", At="Find Agmond", point={x=6593,y=6562}},
@@ -27,8 +29,8 @@ CLGuide_Badlands = {
         {Text="Deliver A Dwarf and His Tools", Dt={q="A Dwarf and His Tools"}, point={x=5342,y=4340}},
         {Text="Deliver A Sign of Hope", Dt={q="A Sign of Hope"}, point={x=5342,y=4340}},
         {Text="Accept A Sign of Hope", At="A Sign of Hope", point={x=5342,y=4340}},
-        {Text="Deliver Mirages", Dt={q="Mirages"}, point={x=5380,y=4330}},
-        {Text="Accept Scrounging", At="Scrounging", point={x=5380,y=4330}},
+        {Text="Deliver Mirages", Dt={q="Mirages"}, point={x=5380,y=4330}, PinAdd="When Mirages is delivered + you are l40, accept Tremors of the earth directly SE of 2dorfs"},
+        {Text="Accept Scrounging", At="Scrounging", point={x=5380,y=4330}, PinAdd="When got tremors, look for Boss Cho'Gun (anticlockwise center to west)"},
         
         -- todo: this requires lvl 40 and Mirages completed. Can we put in a requirement to hit 40 before doing other quests?
         -- would be super, so we dont have to do a dedicated trip all over the map looking for ogre boss dude
@@ -46,6 +48,8 @@ CLGuide_Badlands = {
 
 
         {Text="Complete Murdaloc and Indurium", Mct={"Murdaloc", "Indurium"}, point={x=4963,y=6632}},
+        
+        -- this may not be completed solo, but pickup in case
         {Text="Accept Solution to Doom", At="Solution to Doom", point={x=5139,y=7687}},
 
         {Text="Deliver Indurium", Dt={q="Indurium"}, point={x=4221,y=5268}},
@@ -70,7 +74,6 @@ CLGuide_Badlands = {
         
         -- south-mid elementals
         {Text="Complete Study of the Elements: Rock (Only Lesser elementals)", Ct="Study of the Elements: Rock", point={x=3776,y=7544}},
-
         
         {Text="Deliver Study of the Elements: Rock", Dt={q="Study of the Elements: Rock"}, point={x=2595,y=4486}},
         {Text="Accept Study of the Elements: Rock", At="Study of the Elements: Rock", point={x=2595,y=4486}},
@@ -98,14 +101,33 @@ CLGuide_Badlands = {
         {Text="> Accept Stone Is Better than Cloth", At="Stone Is Better than Cloth", point={x=2582,y=4424}},
         {Text="> Deliver Stone Is Better than Cloth", Dt={q="Stone Is Better than Cloth"}, point={x=2582,y=4424}},
 
+        {Text="Deliver Barbecued Buzzard Wings", Dt={q="Barbecued Buzzard Wings"}, point={x=4239,y=5293}, PinAdd="Vendor at Jazzrik next to delivery guy"},
+        
         {Text="Deliver Scournging", Dt={q="Scrounging", Item="Salbac Shield", Vendor="Salbac Shield"}, point={x=5381,y=4330}},
         -- skip Accept Fiery Blaze Enchantments (2% droprate..) 
 
-      
+        -- groups: definitely consider running thelsamar ironbands excavation site first to turn inn murdaloc/reagent run
+        -- and get uldaman followups 
+
+        {Text="Deliver A Sign of Hope inside uldaman pre-instance", Dt={q="A Sign of Hope"}, point={x=5404,y=5767}, 
+            PinAdd="Solution to doom if possible now"},
+        {Text="Complete Power Stones. Mages squishy, melees not so much (grind outside)", Ct="Power Stones"},
+
+        {Text="Deliver Power Stones (goblin in mid)", Dt={q="Power Stones"}, point={x=4240,y=5294}, PinAdd="Deliver Solution to Doom (SE) if did it, otherwise abandon"},
+
+        -- group: definitely do solution to doom, uldaman reagent run and Agmon's Fate
+        {Text="Run to Loch Modan", Zone="Loch Modan"},
+        {Text="Deliver Murdaloc", Dt={q="Murdaloc"}, point={x=5381,y=4330}},
+        {Text="Hearthstone Thelsamar", UseItem="Hearthstone"},
+        
+        {Text="Deliver Badlands Reagent Run", Dt={q="Badlands Reagent Run"}, point={x=3707,y=4938}},
+        
+        {Text="Fly to SW", Taxi="Stormwind, Elwynn", point={x=3394,y=5096}},
         -- at uldaman do
         -- a sign of hope
         -- solution of doom
         -- power stones
+        -- groups: do more uldaman shit
         {Text="---"},
     }
 }
